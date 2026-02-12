a19016418142bedcfdde37f5593f3367a5fa4aec
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2026-02-12 13:34:32 +0530

    Sidebar created

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Sidebar from './Sidebar.jsx'
+import MessageContainer from './MessageContainer.jsx'
 
 const HomePage = () => {
   return (
-    <div className='text-white'>HomePage</div>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 text-black'>
+      <Sidebar />
+      <MessageContainer/>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BiSearchAlt2} from "react-icons/bi"
+
+const Sidebar = () => {
+  return (
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
+        <form className='flex items-center gap-2'>
+            <input 
+                type="text" className='bg-white text-black input input-bordered rounded-md' placeholder='Search...'
+            />
+            <button type='submit' className='btn text-white bg-zinc-500'>
+                <BiSearchAlt2 className='w-6 h-6 outline-none'/>
+            </button>
+
+        </form>
+        <div className="divider divider-neutral"></div>
+    </div>
+  )
+}
+
+export default Sidebar
