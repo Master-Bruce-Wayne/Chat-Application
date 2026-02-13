53e27695157cb057354a51e0eee6e0579a4d8a4e
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2026-02-13 12:49:14 +0530

    Message Container updated

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import InputSender from './InputSender.jsx'
+import Messages from './Messages.jsx'
 
 const MessageContainer = () => {
   return (
-    <div className='w-full'>
+    <div className='w-full p-2'>
         MessageContainer
+        <Messages/>
         <InputSender/>
     </div>
   )
