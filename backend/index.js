00056119a1396ff7439a8b96843478d99530a4ce
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2026-02-14 06:37:02 +0530

    Socket-io initialised

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,8 @@ import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import {app,server} from "./socket/socket.js"
 dotenv.config({});
-
-const app=express();
 
 const PORT = process.env.PORT || 8080;
 
@@ -28,7 +27,7 @@ app.use("/api/v1/message",messageRoute);
 const startServer = async () => {
     try {
         await connectDB()
-        app.listen(PORT, "0.0.0.0",() => {
+        server.listen(PORT, "0.0.0.0",() => {
             console.log(` Server running on port ${PORT}`)
         })
     } catch (error) {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import io from "socket.io-client";
+import { useState } from 'react';
 
 // components
 import HomePage from './components/HomePage.jsx'
 import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx'
+import { useEffect } from 'react';
+
 
 const router = createBrowserRouter([
   {
@@ -22,6 +27,20 @@ const router = createBrowserRouter([
 ])
 
 function App() {
+  const [socket,setSocket] =useState(null);
+  const {authUser} = useSelector(store=>store.user);
+
+  useEffect(() =>{
+    if(authUser) {
+      const socket=io('http://localhost:8000',{
+        query:{
+          userId: authUser._id
+        }
+      });
+      setSocket(socket);
+    }
+  },[authUser]);
+
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <RouterProvider router={router} />
